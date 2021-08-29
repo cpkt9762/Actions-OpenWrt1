a8d87196ae2d0fbf3d23ad0714a2ecd03b398f34
sxpp316 <sxpp316@gmail.com>
2021-08-29 12:07:41 +0800

    添加主题配置

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,20 +11,20 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 # 一、云编译自定义默认主题：修改[diy-part2.sh]文件
 # 删除原主题：
-rm -rf package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon
 
 # 添加新的主题：
-git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+# # git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
 
 # 取消bootstrap为默认主题：
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 修改bootstrap主题为atmaterial主题：
 # sed -i 's/更改前的主题名/更改后的主题名/g' ./feeds/luci/collections/luci/Makefile
-sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial/g' ./feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial/g' ./feeds/luci/collections/luci/Makefile
 
 # 修改某一文件格式：
 # sed -i 's/更改前的信息/更改后的信息/g' ./要修改的文件的目录（可以用本地查看）
