725f6b16e5682820b8a2e2a03babd5fa7dbbbfb3
check <sxpp316@gmail.com>
2021-09-06 17:55:07 +0800

    去掉修改 Edge 为默认主题

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,4 +33,4 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-R4s'' package/lean/default-settings/files/zzz-default-settings
 
 # 修改 Edge 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile 
+#sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile 
