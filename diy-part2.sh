1b61a5cc9a46bae8fb09305c727bcfa433a075b8
check <sxpp316@gmail.com>
2021-09-06 12:48:20 +0800

    1

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,7 +36,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-R4s'' p
 sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile 
 
 # 修改版本号-webui
-modelmark=R`TZ=UTC-8 date +"%Y-%m-%d-%H:%M:%S" -d +"0"days`'byCheck'
+modelmark=R`TZ=UTC-8 date +"%Y-%m-%d-%H" -d +"0"days`'byCheck'
 sed -i "s/DISTRIB_REVISION='R[0-9]*.[0-9]*.[0-9]*.[0-9]*/DISTRIB_REVISION='$modelmark/g" package/lean/default-settings/files/zzz-default-settings
 
 # Add PushBot全能推送 微信推送改版
